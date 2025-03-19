dbc07abc0029d7eb455a495bd7be59e091bab1e9
LinhHN13 <linhhn13@fe.edu.vn>
2025-03-19 14:12:56 +0700

    update code

---
+++ UserManagement/Controllers/UserController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UserManagement.Models;
+using UserManagement.Repositories;
+
+namespace UserManagement.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(User user)
+        {
+            string username = user.Username;
+            string password = user.Password;
+
+            var _user = _userRepository.GetUserByUsernameAndPassword(username, password);
+
+            if (_user != null)
+            {
+                ViewBag.Username = _user.Username;
+                ViewBag.IsLogin = true;
+                HttpContext.Session.SetString("Username", username);
+                HttpContext.Session.SetString("Fullname", _user.Fullname);
+                HttpContext.Session.SetInt32("IsLogin", 1);
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ UserManagement/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UserManagement.Data
+{
+    public class AppDbContex : DbContext
+    {
+        public AppDbContex(DbContextOptions<AppDbContex> options) : base(options)
+        {
+        }
+        public DbSet<UserManagement.Models.User> Users { get; set; }
+        public DbSet<UserManagement.Models.Role> Roles { get; set; }
+    }
+}
